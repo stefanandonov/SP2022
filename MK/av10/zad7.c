b491a214423e6f08b100fb6d0cab0101f8a1abc5
stefan.a <stefan.a@slicelife.com>
2022-12-23 00:07:41 +0100

    Add tasks from office hours in MK/av10

--- MK/av10/zad7.c
+++ MK/av10/zad7.c
@@ -1,5 +1,41 @@
 #include<stdio.h>
 
-int main () {
+int nzs(int a, int b, int divisor) {
+    if (a == 1 && b == 1) {
+        return 1;
+    } else {
+        if (a % divisor != 0 && b % divisor != 0) {
+            return 1 * nzs(a, b, divisor + 1);
+        } else {
+            if (a % divisor == 0) {
+                a /= divisor;
+            }
+            if (b % divisor == 0) {
+                b /= divisor;
+            }
+            return divisor * nzs(a, b, divisor);
+        }
+    }
+}
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int niza[100];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &niza[i]);
+    }
+
+    int nzsRez = nzs(niza[0], niza[1], 2);
+
+    for (int i = 2; i < n; i++) {
+        nzsRez = nzs(nzsRez, niza[i], 2);
+    }
+
+    printf("%d", nzsRez);
+
+
     return 0;
 }
--- MK/av10/zad8.c
+++ MK/av10/zad8.c
@@ -1,5 +1,45 @@
 #include<stdio.h>
 
-int main () {
+int max(int *array, int n) {
+    if (n == 2) {
+        if (array[0] > array[1]) {
+            return array[0];
+        } else {
+            return array[1];
+        }
+    } else {
+        int maxPrevious = max(array, n - 1);
+        if (array[n - 1] > maxPrevious) {
+            return array[n - 1];
+        } else {
+            return maxPrevious;
+        }
+    }
+}
+
+
+int max2 (int * array, int n, int max) {
+    if (n==0){
+        return max;
+    }
+    if (array[n-1]>max) {
+        return max2(array,n-1, array[n-1]);
+    } else {
+        return max2(array,n-1, max);
+    }
+}
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int niza[100];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &niza[i]);
+    }
+
+    printf("%d", max2(niza, n, niza[n-1]));
+
     return 0;
 }
